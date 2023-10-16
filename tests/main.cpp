e88d7e2eccea7e7ad443fd8be3db3f8034067d27
Lucas Lion <lucaslion7@gmail.com>
2023-10-16 23:26:32 +0200

    still struggling with doctest

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,24 @@
 
-#include "tests.cpp"
+#define DOCTEST_CONFIG_IMPLEMENT #_WITH_MAIN
+#include "doctest/doctest/doctest.h"
 #include <thread>
 
+#include "../includes/header.hpp"
+#include "../includes/Server.hpp"
+#include "../includes/User.hpp"
+#include "../includes/Message.hpp"
+#include "../includes/Channel.hpp"
+
 volatile sig_atomic_t gSignalStatus = 0;
+int g_argc;
+char** g_argv;
 
 void	signal_handler(int signal) {
-	if (signal == SIGINT)
+	if (signal == SIGINT) {
 		gSignalStatus = signal;
+		std::cout << "signal received: " << signal << std::endl;
+		//exit(signal);
+	}
 }
 
 void	runContext(doctest::Context *context) {
@@ -17,21 +29,26 @@ void	runContext(doctest::Context *context) {
 
 int main(int argc , char *argv[])
 {
+    g_argc = argc;
+    g_argv = argv;
 	try {
 		if (argc == 3) {
+			std::cout << "TESTS" << std::endl;
 			doctest::Context context;
+			context.applyCommandLine(argc, argv);
 			Server server(argv[1], argv[2]);
 
 			std::signal(SIGINT, signal_handler);
 
             std::thread contextThread([&context]() {
-                context.run();
+                runContext(&context);
             });
 
-			server.run();
 
-			while (!gSignalStatus)
+			while (!gSignalStatus) {
 				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			}
+			server.run();
 
 			contextThread.join();
 		}
@@ -43,33 +60,13 @@ int main(int argc , char *argv[])
 	return 0;
 }
 
+//TEST_CASE("testing the initialisation function") {
 //
-//#define DOCTEST_CONFIG_IMPLEMENT
-//#include "doctest.h"
-//
-//int main(int argc, char** argv) {
-//    doctest::Context context;
-//
-//    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
-//
-//    // defaults
-//    context.addFilter("test-case-exclude", "*math*"); // exclude test cases with "math" in their name
-//    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
-//    context.setOption("order-by", "name");            // sort the test cases by their name
-//
-//    context.applyCommandLine(argc, argv);
-//
-//    // overrides
-//    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
-//
-//    int res = context.run(); // run
-//
-//    if(context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-//        return res;          // propagate the result of the tests
-//    
-//    int client_stuff_return_code = 0;
-//    // your program - if the testing framework is integrated in your production code
-//    
-//    return res + client_stuff_return_code; // the result from doctest is propagated here as well
+//	doctest::Context context;
+//	context.applyCommandLine(g_argc, g_argv);
+//	program(g_argc, g_argv);
+//	Server server;
+//	//CHECK(server.getPortno() == 6667);
+//	CHECK(server.getPortno() == 0);
+//	CHECK(server.getPortno() == 0);
 //}
-//
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,16 +1,1 @@
 
-#define DOCTEST_CONFIG_IMPLEMENT #_WITH_MAIN
-#include "doctest/doctest/doctest.h"
-
-#include "../includes/header.hpp"
-#include "../includes/Server.hpp"
-#include "../includes/User.hpp"
-#include "../includes/Message.hpp"
-#include "../includes/Channel.hpp"
-
-
-TEST_CASE("testing the initialisation function") {
-	Server server;
-
-	CHECK(server.getPortno() == 6667);
-}
